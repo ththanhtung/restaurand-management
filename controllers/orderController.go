e88dcd7a9da5364e12ffc1a706f9b192f77f6c67
ththanhtung <nguyenththanhtung317@gmail.com>
2023-07-02 18:16:17 +0700

    feat: add create new order feature

--- controllers/orderController.go
+++ controllers/orderController.go
@@ -1,1 +1,43 @@
 package controllers
+
+import (
+	"context"
+	"mongotest/database"
+	"mongotest/models"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var orderCollection *mongo.Collection = database.OpenCollection(database.Client, "orders")
+
+func NewOrder() gin.HandlerFunc{
+	return func(c *gin.Context){
+		var orderReq *models.OrderRequest
+
+		c.ShouldBindJSON(&orderReq)
+
+		order := &models.Order{
+			UserId: orderReq.UserId,
+			TableId: orderReq.TableId,
+		}
+		order.ID = primitive.NewObjectID()
+		order.OrderId = order.ID.Hex()
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10 *time.Second)
+		_, err := orderCollection.InsertOne(ctx, order)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return 			
+		}
+		defer cancel()
+		 
+		c.JSON(http.StatusCreated, order)
+	}
+}
+
--- routes/orderRoute.go
+++ routes/orderRoute.go
@@ -1,11 +1,15 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"mongotest/controllers"
+
+	"github.com/gin-gonic/gin"
+)
 
 func OrderRoutes(incommingRoute *gin.Engine) {
 	incommingRoute.GET("/orders")
 	incommingRoute.GET("/orders/:id")
-	incommingRoute.POST("/orders")
+	incommingRoute.POST("/orders", controllers.NewOrder())
 	incommingRoute.PATCH("/orders/:id")
 	incommingRoute.DELETE("/orders/:id")
 }
