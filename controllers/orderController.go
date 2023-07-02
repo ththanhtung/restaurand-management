b1e19f82b29422b89202d0b61e2c293ba994c65e
ththanhtung <nguyenththanhtung317@gmail.com>
2023-07-02 18:35:43 +0700

    feat: add get all order of a user feature

--- controllers/orderController.go
+++ controllers/orderController.go
@@ -8,36 +8,66 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 var orderCollection *mongo.Collection = database.OpenCollection(database.Client, "orders")
 
-func NewOrder() gin.HandlerFunc{
-	return func(c *gin.Context){
+func NewOrder() gin.HandlerFunc {
+	return func(c *gin.Context) {
 		var orderReq *models.OrderRequest
 
 		c.ShouldBindJSON(&orderReq)
 
 		order := &models.Order{
-			UserId: orderReq.UserId,
+			UserId:  orderReq.UserId,
 			TableId: orderReq.TableId,
 		}
 		order.ID = primitive.NewObjectID()
 		order.OrderId = order.ID.Hex()
 
-		ctx, cancel := context.WithTimeout(context.Background(), 10 *time.Second)
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 		_, err := orderCollection.InsertOne(ctx, order)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{
 				"error": err.Error(),
 			})
-			return 			
+			return
 		}
 		defer cancel()
-		 
+
 		c.JSON(http.StatusCreated, order)
 	}
 }
 
+func GetOrders() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		value, _ := c.Get("user")
+
+		user := value.(*models.User)
+
+		matchStage := bson.D{{"$match", bson.D{{"userid", user.UserID}}}}
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10 *time.Second)
+		cursor, err := orderCollection.Aggregate(ctx, mongo.Pipeline{matchStage})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		defer cancel()
+
+		results := []primitive.M{}
+		if err = cursor.All(ctx, &results); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, results)
+	}
+}
--- routes/orderRoute.go
+++ routes/orderRoute.go
@@ -2,12 +2,13 @@ package routes
 
 import (
 	"mongotest/controllers"
+	"mongotest/middlewares"
 
 	"github.com/gin-gonic/gin"
 )
 
 func OrderRoutes(incommingRoute *gin.Engine) {
-	incommingRoute.GET("/orders")
+	incommingRoute.GET("/orders", middlewares.RequireAuth(), controllers.GetOrders())
 	incommingRoute.GET("/orders/:id")
 	incommingRoute.POST("/orders", controllers.NewOrder())
 	incommingRoute.PATCH("/orders/:id")
