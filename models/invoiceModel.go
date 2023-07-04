883be67a7c3aa222748ca2bc028852dee9b1ef00
ththanhtung <nguyenththanhtung317@gmail.com>
2023-07-04 20:46:19 +0700

    refactor: add invoice model

--- models/invoiceModel.go
+++ models/invoiceModel.go
@@ -1,1 +1,14 @@
 package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type Invoice struct {
+	ID        primitive.ObjectID `bson:"_id"`
+	InvoiceId string
+	OrderId   string
+	Total     string
+}
+
+type InvoiceRequest struct {
+	OrderId string `json:"orderId"`
+}
