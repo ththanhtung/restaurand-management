d695b9715b0083d56aadad6b9d320ca154f940ac
ththanhtung <nguyenththanhtung317@gmail.com>
2023-07-08 23:37:39 +0700

    feat: add cors

--- go.mod
+++ go.mod
@@ -4,9 +4,11 @@ go 1.19
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gin-contrib/cors v1.4.0
 	github.com/gin-gonic/gin v1.9.1
 	github.com/go-playground/validator/v10 v10.14.0
 	github.com/joho/godotenv v1.5.1
+	github.com/stretchr/testify v1.8.4
 	go.mongodb.org/mongo-driver v1.12.0
 	golang.org/x/crypto v0.9.0
 )
@@ -31,7 +33,6 @@ require (
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/testify v1.8.4 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
--- main.go
+++ main.go
@@ -6,10 +6,11 @@ import (
 	"mongotest/routes"
 	"os"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
-func init(){
+func init() {
 	log.Println("init")
 	initializers.LoadEnv()
 }
@@ -19,9 +20,16 @@ func main() {
 
 	port := os.Getenv("PORT")
 
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"}
+	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type"}
+
 	if port == "" {
 		port = "8080"
 	}
+
+	router.Use(cors.New(config))
 
 	routes.FoodRoutes(router)
 	routes.OrderRoutes(router)
@@ -31,5 +39,5 @@ func main() {
 	routes.TableRoutes(router)
 	routes.AuthRoutes(router)
 
-	router.Run(":"+ port)
+	router.Run(":" + port)
 }
